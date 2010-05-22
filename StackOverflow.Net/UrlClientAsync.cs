11eb3020c656a4cc37f4b30073a507c81549fd35
SND\lfoust_cp <SND\lfoust_cp@1e68c9db-d3bb-46d7-945c-fd054d4afafc>
2010-05-22 15:34:38 +0000

    Applied patch by francip to add support for multiple concurrent requests in silverlight
    git-svn-id: https://stackoverflow.svn.codeplex.com/svn/trunk@48514 1e68c9db-d3bb-46d7-945c-fd054d4afafc

--- StackOverflow.Net/UrlClientAsync.cs
+++ StackOverflow.Net/UrlClientAsync.cs
@@ -17,75 +17,85 @@ namespace StackOverflow
     public class UrlClientAsync : IUrlClientAsync
 #endif
     {
-        private Action<HttpResponse> callback;
-        Action<ApiException> onError;
-        private Uri url;
+        private class RequestHelper
+        {
+            public RequestHelper(Uri url, Action<HttpResponse> callback, Action<ApiException> onError)
+            {
+                this.callback = callback;
+                this.url = url;
+                this.onError = onError;
+            }
+
+            private Action<HttpResponse> callback;
+            Action<ApiException> onError;
+            private Uri url;
+
+#if !SILVERLIGHT
+            public void client_DownloadDataCompleted(object sender, DownloadDataCompletedEventArgs e)
+            {
+                if (e.Error != null)
+                {
+                    if (onError != null)
+                        onError(new ApiException(null, e.Error));
+                    return;
+                }
+
+                using (var memoryStream = new MemoryStream(e.Result))
+                {
+                    using (var stream = new GZipStream(memoryStream, CompressionMode.Decompress))
+                    {
+                        var decompressed = new List<byte>();
+                        int i;
+                        while ((i = stream.ReadByte()) != -1)
+                            decompressed.Add((byte)i);
+                        string decoded = Encoding.UTF8.GetString(decompressed.ToArray());
+                        var response = new HttpResponse
+                        {
+                            Body = decoded,
+                            Url = url
+                        };
+                        callback(response);
+                    }
+                }
+            }
+#endif
+
+            public void client_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+            {
+                if (e.Error != null)
+                {
+                    if (onError != null)
+                        onError(new ApiException(null, e.Error));
+                    return;
+                }
+
+                var response = new HttpResponse
+                {
+                    Body = e.Result,
+                    Url = url
+                };
+                callback(response);
+            }
+        }
 
         public void MakeRequest(Uri url, Action<HttpResponse> callback, Action<ApiException> onError)
         {
-            this.callback = callback;
-            this.url = url;
-            this.onError = onError;
-
             var client = new HttpClient();
+            var requestHelper = new RequestHelper(url, callback, onError);
+
             //TODO: Set the User Agent string to something useful
             //client.Headers[HttpRequestHeader.UserAgent] = "";
 #if !SILVERLIGHT
             client.Headers[HttpRequestHeader.AcceptEncoding] = "gzip";
             client.Encoding = Encoding.UTF8;
-            client.DownloadDataCompleted += new DownloadDataCompletedEventHandler(client_DownloadDataCompleted);
+            client.DownloadDataCompleted += new DownloadDataCompletedEventHandler(requestHelper.client_DownloadDataCompleted);
             client.DownloadDataAsync(url);
 #else
-            client.DownloadStringCompleted += new DownloadStringCompletedEventHandler(client_DownloadStringCompleted);
+            // TODO: This assumes that the client app will not start new web request before the previous one has completed
+            // Need to create a wrapper instance per request
+            client.DownloadStringCompleted += new DownloadStringCompletedEventHandler(requestHelper.client_DownloadStringCompleted);
             client.DownloadStringAsync(url);
 #endif
-        }
-
-#if !SILVERLIGHT
-        void client_DownloadDataCompleted(object sender, DownloadDataCompletedEventArgs e)
-        {
-            if (e.Error != null)
-            {
-                if (onError != null)
-                    onError(new ApiException(null, e.Error));
-                return;
-            }
-
-            using (var memoryStream = new MemoryStream(e.Result))
-            {
-                using (var stream = new GZipStream(memoryStream, CompressionMode.Decompress))
-                {
-                    var decompressed = new List<byte>();
-                    int i;
-                    while ((i = stream.ReadByte()) != -1)
-                        decompressed.Add((byte)i);
-                    string decoded = Encoding.UTF8.GetString(decompressed.ToArray());
-                    var response = new HttpResponse
-                    {
-                        Body = decoded,
-                        Url = url
-                    };
-                    callback(response);
-                }
-            }
-        }
-#endif
-
-        void client_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
-        {
-            if (e.Error != null)
-            {
-                if (onError != null)
-                    onError(new ApiException(null, e.Error));
-                return;
-            }
-
-            var response = new HttpResponse
-            {
-                Body = e.Result,
-                Url = url
-            };
-            callback(response);
         }
     }
 }

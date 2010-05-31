ca95fa61da58430aeb3a581bb4329fdab148e77d
SND\TheHurt_cp <SND\TheHurt_cp@1e68c9db-d3bb-46d7-945c-fd054d4afafc>
2010-05-31 04:48:57 +0000

    Initial checkin of the SO MVC sample.
    git-svn-id: https://stackoverflow.svn.codeplex.com/svn/trunk@50093 1e68c9db-d3bb-46d7-945c-fd054d4afafc

---
+++ Samples/StackOverflow.Net.Mvc/StackOverflow.Net.Mvc/Models/StackOverflowNetRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+using StackOverflow.Net;
+
+namespace StackOverflow.Net.Mvc.Models
+{
+    public class StackOverflowNetRepository
+    {
+        private string _apiVersion = string.Empty;
+        private string _apiKey = string.Empty;
+        IUrlClient _urlClient = null;
+        IProtocol _protocol = null;
+        StackOverflowClient _client = null;
+
+        private StackOverflowNetRepository()
+        {
+            _apiVersion = ConfigurationManager.AppSettings["StackOverflow.Net.ApiVersion"];
+            _apiKey = ConfigurationManager.AppSettings["StackOverflow.Net.ApiKey"];
+            _urlClient = new UrlClient();
+            _protocol = new JsonProtocol();
+        }
+
+        public StackOverflowNetRepository(SiteState state) : this()
+        {
+            _client = new StackOverflowClient(_apiVersion, _apiKey, state.HostSite, _urlClient, _protocol);
+        }
+
+        public IPagedList<Question> GetQuestions(QuestionSort sortBy = QuestionSort.Active, SortDirection sortDirection = SortDirection.Descending, int? page = null, int? pageSize = null, bool includeBody = false, bool includeComments = false, DateTime? fromDate = null, DateTime? toDate = null, string[] tags = null)
+        {
+            return _client.GetQuestions(sortBy, sortDirection, page, pageSize, includeBody, includeComments, fromDate, toDate, tags);
+        }
+    }
+}
